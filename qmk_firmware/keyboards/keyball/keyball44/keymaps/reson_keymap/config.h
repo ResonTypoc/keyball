2ca1ef0b8461f1f7410ffea640ac393dc7735771
Reson <reson.typoc.1260@gmail.com>
2025-05-05 00:42:38 +0900

    スクロール速度を6に

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/reson_keymap/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/reson_keymap/config.h
@@ -41,4 +41,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 
 #define TAPPING_TERM 180
-#define KEYBALL_SCROLL_DIV_DEFAULT 2
+#define KEYBALL_SCROLL_DIV_DEFAULT 6
