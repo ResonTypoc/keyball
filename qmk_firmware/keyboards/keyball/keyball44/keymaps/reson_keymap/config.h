60e569e728807b43e26dd6ff6830d656fc5fa04b
Reson <reson.typoc.1260@gmail.com>
2025-05-22 22:05:55 +0900

    オートマウスレイヤーを有効化, レイヤ数を7に変更

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/reson_keymap/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/reson_keymap/config.h
@@ -35,10 +35,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
-// #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-// #define AUTO_MOUSE_DEFAULT_LAYER 2
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 7
 
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
 
 #define TAPPING_TERM 164
 #define KEYBALL_SCROLL_DIV_DEFAULT 6
