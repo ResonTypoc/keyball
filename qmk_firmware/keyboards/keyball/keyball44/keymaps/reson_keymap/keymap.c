5ff5e8230789c54d584cf8132ef17d51dd2be409
Reson <reson.typoc.1260@gmail.com>
2025-05-31 00:38:10 +0900

    キーマップ調整

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/reson_keymap/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/reson_keymap/keymap.c
@@ -31,10 +31,10 @@ enum my_keyball_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U           , KC_I     , KC_O           , KC_P                 , KC_BSPC      ,
-    MT(MOD_LCTL, KC_TAB), KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                             KC_H     , KC_J           , KC_K     , KC_L           , LT(2,KC_SCLN)        , LT(3,KC_ENT) ,
+    KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U           , KC_I     , KC_O           , KC_P                 , KC_BSPC      ,
+    MOD_LCTL , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J           , KC_K     , KC_L           , LT(2,KC_SCLN)        , LT(3,KC_ENT) ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M           , KC_COMM  , KC_DOT         , MT(MOD_LSFT,KC_SLSH) , KC_BSLS      ,
-               KC_LALT  , KC_LGUI             , LT(4,KC_F20) , LT(3,KC_SPC) , LT(5,KC_MINS)     , MT(MOD_LGUI,KC_GRV) , LT(4,KC_F21)   , _______  , _______        , LT(1,KC_DELETE)
+               KC_LALT  , KC_LGUI             , LT(4,KC_F20) , LT(3,KC_SPC) , LT(5,KC_MINS)     , MT(MOD_LGUI,KC_GRV) , LT(4,KC_F21)   , _______  , _______        , LT(1,KC_ESC)
   ),
 
   [1] = LAYOUT_universal(
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT_universal(
-    KC_TRNS  ,KC_TRNS          , KC_TRNS  , KC_TRNS  , KC_TRNS  , KC_TRNS     ,                              CPI_I1K  , KC_F5      , PRC_TOG    , KC_TRNS , CPI_I100 , KC_TRNS,
-    KC_TRNS  ,KC_TRNS          , KC_TRNS  , KC_TRNS  , KC_TRNS  , KC_TRNS     ,                              CPI_D1K  , KC_MS_BTN1 , KC_MS_BTN2 , PRC_SW  , CPI_D100 , KC_TRNS,
+    KC_TRNS  ,KC_TRNS          , KC_TRNS  , KC_TRNS  , KC_TRNS  , KC_TRNS     ,                              KC_TRNS  , KC_F5      , KC_TRNS    , KC_TRNS , KC_TRNS , KC_TRNS,
+    KC_TRNS  ,KC_TRNS          , KC_TRNS  , KC_TRNS  , KC_TRNS  , KC_TRNS     ,                              KC_TRNS  , KC_MS_BTN1 , KC_MS_BTN2 , PRC_SW  , KC_TRNS , KC_TRNS,
     KC_TRNS  ,KC_TRNS          , KC_TRNS  , KC_TRNS  , KC_TRNS  , KC_TRNS     ,                              KC_TRNS  , KC_MS_BTN4 , KC_MS_BTN5 , KC_TRNS , KC_TRNS , KC_TRNS,
               KC_TRNS          , KC_TRNS  , KC_TRNS  , KC_TRNS  , KC_TRNS     ,                   KC_TRNS   , KC_TRNS , _______    , _______              , KC_TRNS
   ),
